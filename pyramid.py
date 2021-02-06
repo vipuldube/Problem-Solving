45f9ca064e726635695dd23bae98bc5718d1edfb
Vipul Dubey <77088016+vipuldube@users.noreply.github.com>
2021-02-06 11:40:25 +0530

    Add files via upload
    pyramid

---
+++ pyramid.py
@@ -0,0 +1,13 @@
+# pyramid in python
+j = 9
+for i in range (1,10,2):
+    print(' '*j+i*'*')
+    j = j-1
+
+  # Reverse pyramid in python
+    i = 1
+    for j in range(11, 0, -2):
+        print(i * ' ' + j * '*')
+        i = i + 1
+
+
